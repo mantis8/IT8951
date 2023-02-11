73736534a6b6080087aa899a3fbc63c16fc13f96
Manuel <m-tischhauser@hotmail.com>
2023-02-11 18:30:54 +0100

    declarations added

--- lib/IT8951/src/IT8951.h
+++ lib/IT8951/src/IT8951.h
@@ -11,8 +11,36 @@
 #ifndef MATI_IT8951_H
 #define MATI_IT8951_H
 
+#include "ISpi.h"
+#include "IGpio.h"
+
 namespace mati {
 
+template<uint32_t BufferSize>
+class IT8951 {
+  public:
+    enum class Status {
+        ok = 0,
+        busy,
+        error
+    };
+
+    IT8951(hardware_abstraction::ISpi& spi, hardware_abstraction::IGpio& resetGpo, hardware_abstraction::IGpio& busyGpi);
+    IT8951(const IT8951&) = delete;
+    IT8951(IT8951&&) = delete;
+    operator=(const IT8951&) = delete;
+    operator=(IT8951&&) = delete;
+
+    Status wakeUp();
+    Status standby();
+    void reset();
+    // TODO DeviceInfo    
+    Status setVcom(float vcom);
+    
+  
+  private:
+
+};
 } // mati
 
 #endif // MATI_IT8951_H
