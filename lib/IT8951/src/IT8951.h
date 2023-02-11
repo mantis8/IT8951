8ae1a63a401964c27132eab1a7de05bf89f2f933
Manuel <m-tischhauser@hotmail.com>
2023-02-11 16:01:25 +0100

    IT8951 files added to poject

---
+++ lib/IT8951/src/IT8951.h
@@ -0,0 +1,18 @@
+//
+// Copyright (c) 2023 Manuel Tischhauser
+//
+// Distributed under the MIT Software License
+// See accompanying file LICENSE or copy at
+// https://opensource.org/licenses/MIT
+//
+// See the documentation for more information
+//
+
+#ifndef MATI_IT8951_H
+#define MATI_IT8951_H
+
+namespace mati {
+
+} // mati
+
+#endif // MATI_IT8951_H
