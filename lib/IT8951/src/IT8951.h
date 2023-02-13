d94641d1c2bc2d9b879a4a224e0bc457a56f4c43
Manuel <m-tischhauser@hotmail.com>
2023-02-13 21:23:59 +0100

    declarations added

--- lib/IT8951/src/IT8951.h
+++ lib/IT8951/src/IT8951.h
@@ -11,6 +11,10 @@
 #ifndef MATI_IT8951_H
 #define MATI_IT8951_H
 
+#include <string>
+#include <cstdint>
+#include <span>
+
 #include "ISpi.h"
 #include "IGpio.h"
 
@@ -25,6 +29,19 @@ class IT8951 {
         error
     };
 
+    struct DeviceInfo() {
+        DeviceInfo(uint16_t width, uint16_t height, uint32_t imageBufferAddress,
+                   std::string firmwareVersion, std::string lutVersion) : 
+                   width_{width}, height_{height}, imageBufferAddress_{imageBufferAddress},
+                   firmwareVersion_{firmwareVersion}, lutVersion_{lutVersion} {};
+
+        const uint16_t width_;
+        const uint16_t height_;
+        const uint16_t imageBufferAddress_;
+        const std::string firmwareVersion_;
+        const std::string lutVersion_;
+    };
+
     IT8951(hardware_abstraction::ISpi& spi, hardware_abstraction::IGpio& resetGpo, hardware_abstraction::IGpio& busyGpi);
     IT8951(const IT8951&) = delete;
     IT8951(IT8951&&) = delete;
@@ -34,11 +51,22 @@ class IT8951 {
     Status wakeUp();
     Status standby();
     void reset();
-    // TODO DeviceInfo    
+    DeviceInfo getDeviceInfo();   
     Status setVcom(float vcom);
+    // TODO: document the following
+    Status writeData(const uint32_t imageBufferAddress, const std::span<uint16_t> data, const uint16_t yCoordinate,
+                     const uint16_t, xCoordinate, const uint16_t width, const uint16_t height);
+    Status display(const uint16_t xCoordinate, const uint16_t yCoordinate, const uint16_t width, const uint16_t height);
     
   
   private:
+    float getVcom(); // TODO: is this needed?
+    Status enablePackedMode();
+    Status disablePackedMode(); // TODO: is this needed?
+
+    hardware_abstraction::ISpi& spi;
+    harware_abstraction::IGpio& resetGpo;
+    hardware_abstraction::IGpio& busyGpi;
 
 };
 } // mati
