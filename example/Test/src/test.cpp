9e9f2e9f41a40a7fae570cf8e6f2236f802fe25b
Manuel <m-tischhauser@hotmail.com>
2023-03-05 11:45:17 +0100

    reset pin and checks fixed

--- example/Test/src/test.cpp
+++ example/Test/src/test.cpp
@@ -14,7 +14,7 @@ TEST_CASE("sadfd") {
         mati::hardware_abstraction::Spi spi{};
         spi.setClockFrequency(2*1000*1000u);
 
-        mati::hardware_abstraction::Gpio resetPin{8, mati::hardware_abstraction::Gpio::Functionality::output};
+        mati::hardware_abstraction::Gpio resetPin{17, mati::hardware_abstraction::Gpio::Functionality::output};
         mati::hardware_abstraction::Gpio busyPin{24, mati::hardware_abstraction::Gpio::Functionality::input};
     
         constexpr uint32_t bufferSize = 300;
@@ -22,9 +22,9 @@ TEST_CASE("sadfd") {
 
         it8951.reset();
 
-        CHECK(mati::IT8951<bufferSize>::Status::ok != it8951.sleep());
-        CHECK(mati::IT8951<bufferSize>::Status::ok != it8951.standby());
-        CHECK(mati::IT8951<bufferSize>::Status::ok != it8951.wakeUp());
+        CHECK(mati::IT8951<bufferSize>::Status::ok == it8951.sleep());
+        CHECK(mati::IT8951<bufferSize>::Status::ok == it8951.standby());
+        CHECK(mati::IT8951<bufferSize>::Status::ok == it8951.wakeUp());
 
 
     } catch (std::runtime_error& e) {
