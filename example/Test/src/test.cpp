9459466e7dcc7b761921f27dacfc3f6c7fa45ef0
Manuel <m-tischhauser@hotmail.com>
2023-04-01 14:51:23 +0200

    reset after standby

--- example/Test/src/test.cpp
+++ example/Test/src/test.cpp
@@ -12,7 +12,7 @@
 TEST_CASE("Case 1") {
     try {
         mati::hardware_abstraction::Spi spi{};
-        spi.setClockFrequency(2*1000*1000u);
+        spi.setClockFrequency(2*1000*1000);
 
         mati::hardware_abstraction::Gpio resetPin{17, mati::hardware_abstraction::Gpio::Functionality::output};
         mati::hardware_abstraction::Gpio busyPin{24, mati::hardware_abstraction::Gpio::Functionality::input};
@@ -20,10 +20,11 @@ TEST_CASE("Case 1") {
         constexpr uint32_t bufferSize = 300;
         mati::IT8951<bufferSize> it8951{spi, resetPin, busyPin};
 
-        it8951.reset();
-
         CHECK(mati::IT8951<bufferSize>::Status::ok == it8951.sleep());
         CHECK(mati::IT8951<bufferSize>::Status::ok == it8951.standby());
+
+        it8951.reset();
+        
         CHECK(mati::IT8951<bufferSize>::Status::ok == it8951.wakeUp());
 
         // device info
