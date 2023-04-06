ee8aa7b28f34858d924d341c579fe3e4adaeab25
Manuel <m-tischhauser@hotmail.com>
2023-04-06 20:08:18 +0200

    display, clear, writeImageBuffer, isDisplayReady, enableParameterPack implemented

--- lib/IT8951/src/IT8951.h
+++ lib/IT8951/src/IT8951.h
@@ -60,7 +60,9 @@ class IT8951 {
     std::tuple<Status, DeviceInfo> getDeviceInfo();   
     Status setVcom(const float vcom);
     std::tuple<Status, float> getVcom();
-    Status writeImage(const std::span<uint16_t> image, const uint16_t xCoordinate, const uint16_t yCoordinate, const uint16_t width, const uint16_t height);
+    Status enableParameterPack();
+    Status writeImage(const uint32_t imageBufferAddress, const std::span<uint16_t> image, const uint16_t xCoordinate, const uint16_t yCoordinate, const uint16_t width, const uint16_t height);
+    std::tuple<Status, bool> isDisplayReady();
     Status display(const uint16_t xCoordinate, const uint16_t yCoordinate, const uint16_t width, const uint16_t height);
     Status clear(const uint16_t xCoordinate, const uint16_t yCoordinate, const uint16_t width, const uint16_t height);
 
@@ -80,16 +82,19 @@ class IT8951 {
     static constexpr uint16_t cDisplayArea_{0x0034};
     static constexpr uint16_t cSetVcom_{0x0039};
     static constexpr uint16_t cGetDeviceInfo_{0x0302};
+    // refresh modes
+    static constexpr uint16_t cClear_{0x0000};
+    static constexpr uint16_t cGrayscale_{0x0002};
+
 
     Status writeCommand(const uint16_t command);
     Status writeCommand(const uint16_t command, const std::span<uint16_t> parameters);
     Status writeData(const std::span<uint16_t> buffer);
     Status readData(const std::span<uint16_t> buffer);
     Status writeRegister(const uint16_t address, const uint16_t value);
-    Status readRegister(const uint16_t address, uint16_t& value);
+    std::tuple<Status, uint16_t> readRegister(const uint16_t address);
+    Status refresh(const uint16_t xCoordinate, const uint16_t yCoordinate, const uint16_t width, const uint16_t height, const uint16_t mode);
     void waitUntilIdle();
-    Status enablePackedMode(); // TODO: is this needed?
-    Status disablePackedMode(); // TODO: is this needed?
 
     std::array<uint16_t, BufferSize> txBuffer_;
     std::array<uint16_t, BufferSize> rxBuffer_;
@@ -167,7 +172,7 @@ IT8951<BufferSize>::Status IT8951<BufferSize>::setVcom(const float vcom) {
         return Status::error;
     }
 
-    std::array<uint16_t, 2> parameters;
+    std::array<uint16_t, 2> parameters{};
 
     parameters.at(0) = 0x0001; // parameter to 1 = set VCOM
     parameters.at(1) = static_cast<uint16_t>(vcom * (-1000.0f)); // the VCOM value converted as described in the programming guide
@@ -177,7 +182,7 @@ IT8951<BufferSize>::Status IT8951<BufferSize>::setVcom(const float vcom) {
 
 template<size_t BufferSize>
 std::tuple<typename IT8951<BufferSize>::Status, float> IT8951<BufferSize>::getVcom() {
-    std::array<uint16_t, 1> parameters;
+    std::array<uint16_t, 1> parameters{};
     parameters.at(0) = 0x0000; // parameter to 0 = get VCOM
     
     auto result = writeCommand(cSetVcom_, parameters);
@@ -185,7 +190,7 @@ std::tuple<typename IT8951<BufferSize>::Status, float> IT8951<BufferSize>::getVc
         return {result, 0.0f};
     }
 
-    std::array<uint16_t, 1> buffer;
+    std::array<uint16_t, 1> buffer{};
 
     result = readData(buffer);
     if (Status::ok != result) {
@@ -198,18 +203,69 @@ std::tuple<typename IT8951<BufferSize>::Status, float> IT8951<BufferSize>::getVc
 }
 
 template<size_t BufferSize>
-IT8951<BufferSize>::Status IT8951<BufferSize>::writeImage(const std::span<uint16_t> image, const uint16_t xCoordinate, const uint16_t yCoordinate, const uint16_t width, const uint16_t height) {
-    // TODO
+IT8951<BufferSize>::Status IT8951<BufferSize>::enableParameterPack() {
+    static constexpr uint16_t address = 0x0004;
+    static constexpr uint16_t value = 0x0001;
+
+    return writeRegister(address, value);
+}
+
+template<size_t BufferSize>
+std::tuple<typename IT8951<BufferSize>::Status, bool> IT8951<BufferSize>::isDisplayReady() {
+    uint16_t value = 42;
+    Status result{Status::error};
+
+    // read from the LUT register
+    static constexpr uint16_t address = 0x1224;
+    std::tie(result, value) = readRegister(address);
+
+    const bool ready = 0x0000 == value;
+    return {result, ready};
+}
+
+template<size_t BufferSize>
+IT8951<BufferSize>::Status IT8951<BufferSize>::writeImage(const uint32_t imageBufferAddress, const std::span<uint16_t> image, const uint16_t xCoordinate, const uint16_t yCoordinate, const uint16_t width, const uint16_t height) {
+    // the programming guide recommends to set the image buffer address with every image load
+    static constexpr uint16_t registerBase = 0x208;
+    auto result = writeRegister(registerBase+2u, static_cast<uint16_t>(imageBufferAddress>>16));
+    if (Status::ok != result) {
+        return result;
+    }
+
+    result = writeRegister(registerBase, static_cast<uint16_t>(imageBufferAddress));
+    if (Status::ok != result) {
+        return result;
+    }
+
+    // TODO change bpp to 4!!!
+    std::array<uint16_t, 5> parameters{};
+    parameters.at(0) = 0x0030; // 0x0{endianesss}{bbp}{rotate}
+    parameters.at(1) = xCoordinate;
+    parameters.at(2) = yCoordinate;
+    parameters.at(3) = width;
+    parameters.at(4) = height;
+
+    result = writeCommand(cLoadImageArea_, parameters);
+    if (Status::ok != result) {
+        return result;
+    }
+
+    result = writeData(image);
+    if (Status::ok != result) {
+        return result;
+    }
+
+    return writeCommand(cEndLoadImage_);
 }
 
 template<size_t BufferSize>
 IT8951<BufferSize>::Status IT8951<BufferSize>::display(const uint16_t xCoordinate, const uint16_t yCoordinate, const uint16_t width, const uint16_t height) {
-    // TODO
+    return refresh(xCoordinate, yCoordinate, width, height, cGrayscale_);
 }
 
 template<size_t BufferSize>
 IT8951<BufferSize>::Status IT8951<BufferSize>::clear(const uint16_t xCoordinate, const uint16_t yCoordinate, const uint16_t width, const uint16_t height) {
-    // TODO
+    return refresh(xCoordinate, yCoordinate, width, height, cClear_);
 }
 
 template<size_t BufferSize>
@@ -327,12 +383,44 @@ IT8951<BufferSize>::Status IT8951<BufferSize>::readData(const std::span<uint16_t
 
 template<size_t BufferSize>
 IT8951<BufferSize>::Status IT8951<BufferSize>::writeRegister(const uint16_t address, const uint16_t value) {
-    // TODO
+    std::array<uint16_t, 2> parameters;
+    parameters.at(0) =  address;
+    parameters.at(1) = value;
+    
+    return writeCommand(cWriteRegister_, parameters);
 }
 
 template<size_t BufferSize>
-IT8951<BufferSize>::Status IT8951<BufferSize>::readRegister(const uint16_t address, uint16_t& value) {
-    // TODO
+std::tuple<typename IT8951<BufferSize>::Status, uint16_t> IT8951<BufferSize>::readRegister(const uint16_t address) {   
+    std::array<uint16_t, 1> parameters;
+    parameters.at(0) = address;
+
+    auto result = writeCommand(cReadRegister_, parameters);
+    if (Status::ok != result) {
+        return result;
+    }
+
+    std::array<uint16_t, 1> buffer;
+    buffer.at(0) = 0;
+
+    result = readData(buffer);
+    if (Status::ok != result) {
+        return result;
+    }
+
+    return {Status::ok, buffer.at(0)};
+}
+
+template<size_t BufferSize>
+IT8951<BufferSize>::Status IT8951<BufferSize>::refresh(const uint16_t xCoordinate, const uint16_t yCoordinate, const uint16_t width, const uint16_t height, const uint16_t mode) {
+    std::array<uint16_t, 5> parameters{};
+    parameters.at(0) = xCoordinate;
+    parameters.at(1) = yCoordinate;
+    parameters.at(2) = width;
+    parameters.at(3) = height;
+    parameters.at(4) = mode;
+
+    return writeCommand(cDisplayArea_, parameters);
 }
 
 template<size_t BufferSize>
